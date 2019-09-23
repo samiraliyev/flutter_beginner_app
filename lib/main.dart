74426ad942c7ba8047f269ce5e2d9828e73ef034
samiraliyev <samir.aliyev@code.edu.az>
2019-09-23 18:18:04 +0400

    new main file
    this is appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:ui';
 
 
-void main(){
+void main() {
   runApp(
-    //MyStatelessWidget()
-      MyStatefulWidget()
-  );
-}
+    MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+} 
 
 
-class MyStatefulWidget extends StatefulWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      child: Center(
-        child: RaisedButton(
-          child: Text("Click", textDirection: TextDirection.ltr,),
-          onPressed: (){
+class _MyStatefulWidgetState extends State<MyApp> {
 
-          },
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Tricks", textDirection: TextDirection.ltr,),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.beenhere),
+            onPressed: (){
+              print("icon");
+            },
+          ),
+           IconButton(
+            icon: Icon(Icons.map),
+            onPressed: (){
+              print("icon");
+            },
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Image(
+                  image: AssetImage("images/bg.jpg"),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0, left: 16.0),
+                  child: CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage("images/samir.jpg"),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 140.0, left: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Samir ALiyev", style: TextStyle(fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                      color: Colors.white)),
+                      Text("samir.aliyev@code.edu.az", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.add),
+                  title: Text("Flutter"),
+                  onTap: (){
+
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.add),
+                  title: Text("Kotlin"),
+                  onTap: (){
+
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.add),
+                  title: Text("React Native"),
+                  onTap: (){
+
+                  },
+                )
+              ],
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          print("fab");
+        },
+      ),
     );
+    
   }
 }
-
-
-
-
-
-/*
-class MyStatelessWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.red,
-        child: Center(
-            child: Text("Hallo from flutter", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 30.0, color: Colors.white))
-        )
-    );
-  }
-}
-*/
